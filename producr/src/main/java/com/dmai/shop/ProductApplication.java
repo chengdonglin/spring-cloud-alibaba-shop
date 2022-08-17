a7dfb290e9d2659ab7d71923bd1245027817105e
linchengdong <chengdong2518@163.com>
2022-08-17 14:59:15 +0800

    商品服务接入 nacos 注册中心

--- producr/src/main/java/com/dmai/shop/ProductApplication.java
+++ producr/src/main/java/com/dmai/shop/ProductApplication.java
@@ -3,6 +3,7 @@ package com.dmai.shop;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @MapperScan("com.dmai.shop.mapper")
 @EnableTransactionManagement(proxyTargetClass = true)
+@EnableDiscoveryClient
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class,args);
