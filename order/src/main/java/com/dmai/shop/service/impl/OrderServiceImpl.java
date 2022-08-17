71ceeb545e79f4c6c3dbb26ee11180bda6bb1800
linchengdong <chengdong2518@163.com>
2022-08-17 15:13:24 +0800

    订单服务应用 DiscoveryClient 服务发现功能

--- order/src/main/java/com/dmai/shop/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/dmai/shop/service/impl/OrderServiceImpl.java
@@ -13,6 +13,8 @@ import com.dmai.shop.utils.constants.HttpCode;
 import com.dmai.shop.utils.resp.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -31,6 +33,10 @@ import java.math.BigDecimal;
 @Slf4j
 public class OrderServiceImpl implements OrderService {
 
+    private static final String USER_SERVER = "shop-user";
+
+    private static final String PRODUCT_SERVER = "shop-product";
+
     @Autowired
     private RestTemplate restTemplate;
 
@@ -40,17 +46,24 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderItemMapper orderItemMapper;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void saveOrder(OrderParams orderParams) {
         if (orderParams == null) {
             throw new RuntimeException("参数异常");
         }
-        User user = restTemplate.getForObject("http://localhost:8060/user/get/" + orderParams.getUserId(), User.class);
+        String userServiceUrl = this.getServiceUrl(USER_SERVER);
+        String productServiceUrl = this.getServiceUrl(PRODUCT_SERVER);
+        // User user = restTemplate.getForObject("http://localhost:8060/user/get/" + orderParams.getUserId(), User.class);
+        User user = restTemplate.getForObject("http://" + userServiceUrl + "/user/get/" + orderParams.getUserId(), User.class);
         if (user == null) {
             throw new RuntimeException("未读取到用户信息:" + JSONObject.toJSONString(orderParams));
         }
-        Product product = restTemplate.getForObject("http://localhost:8070/product/get/" + orderParams.getProductId(), Product.class);
+        // Product product = restTemplate.getForObject("http://localhost:8070/product/get/" + orderParams.getProductId(), Product.class);
+        Product product = restTemplate.getForObject("http://" + productServiceUrl + "/product/get/" + orderParams.getProductId(), Product.class);
         if (product == null) {
             throw new RuntimeException("未读取到商品信息:" + JSONObject.toJSONString(orderParams));
         }
@@ -73,10 +86,22 @@ public class OrderServiceImpl implements OrderService {
         item.setProPrice(product.getProPrice());
         orderItemMapper.insert(item);
 
-        Result<Integer> result = restTemplate.getForObject("http://localhost:8070/product/update_count/" + orderParams.getProductId() + "/" + orderParams.getCount(), Result.class);
+        //Result<Integer> result = restTemplate.getForObject("http://localhost:8070/product/update_count/" + orderParams.getProductId() + "/" + orderParams.getCount(), Result.class);
+        Result<Integer> result = restTemplate.getForObject("http://" + productServiceUrl + "/product/update_count/" + orderParams.getProductId() + "/" + orderParams.getCount(), Result.class);
         if (result.getCode() != HttpCode.SUCCESS) {
             throw new RuntimeException("扣减库存失败");
         }
         log.info("扣减库存成功");
+    }
+
+
+    /**
+     * 获取动态服务地址
+     * @param serviceName
+     * @return
+     */
+    private String  getServiceUrl(String serviceName) {
+        ServiceInstance serviceInstance = discoveryClient.getInstances(serviceName).get(0);
+        return serviceInstance.getHost() + ":" + serviceInstance.getPort();
     }
 }
